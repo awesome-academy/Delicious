3f2938a9bfd0506cb7dd461883c4bbc5891e62d9
HoaPQ <rubik0403@gmail.com>
2020-08-04 13:08:15 +0700

    Add Coredata base

---
+++ Delicious/Extensions/MagicalRecord+Rx.swift
@@ -0,0 +1,32 @@
+//
+//  MagicalRecord+Rx.swift
+//  Delicious
+//
+//  Created by HoaPQ on 8/4/20.
+//  Copyright © 2020 HoaPQ. All rights reserved.
+//
+
+import MagicalRecord
+
+extension Reactive where Base: MagicalRecord {
+    static func save(block: @escaping (NSManagedObjectContext?) -> Void) -> Observable<Bool> {
+        return Observable.create { observer in
+            MagicalRecord.save(block, completion: { (changed, error) in
+                if let error = error {
+                    observer.onError(error)
+                } else {
+                    observer.onNext(changed)
+                    observer.onCompleted()
+                }
+            })
+            return Disposables.create {
+                observer.onCompleted()
+            }
+        }
+    }
+    
+    static func save(block: @escaping (NSManagedObjectContext?) -> Void) -> Observable<Void> {
+        let observable: Observable<Bool> = MagicalRecord.rx.save(block: block)
+        return observable.map { _ in () }
+    }
+}
